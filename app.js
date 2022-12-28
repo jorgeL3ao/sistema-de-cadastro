90e195d706afd5b23b0fbeba6ba9ac71ba57be17
jorge <jorgesalgado1190@gmail.com>
2022-12-28 19:16:07 -0300

    Alterando o arquivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("A aplicacao esta sendo conectada com o banco de dados")
+// Mais um console.log de teste observe
+console.log("Obeserve essse teste")
